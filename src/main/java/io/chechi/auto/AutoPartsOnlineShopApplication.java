a990bc1660beea6fe108ea775579b00fc43a11ea
xChechi <stefannasev81@gmail.com>
2023-11-17 10:09:51 +0000

    1st commit

---
+++ src/main/java/io/chechi/auto/AutoPartsOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package io.chechi.auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPartsOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPartsOnlineShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/chechi/auto/AutoPartsOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chechi.auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPartsOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
